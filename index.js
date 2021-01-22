af7fa033f92d5d6bdbe40821a90cb532ed264844
Cauê Henrique <caue313@gmail.com>
2021-01-22 18:27:24 -0300

    Added more genres and environments.

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 const discord = require("discord.js");
 const client = new discord.Client();
 
-var envArray = ["Forest", "Supermarket", "Medieval Castle", "Ocean", "Jungle", "Cave", "City", "Farm", "Beach"];
-var genreArray = ["FPS", "Action RPG", "Turn RPG", "Visual Novel", "Top-Down Shooter", "Platformer", "Soccer", "Strategy", "Adventure", "Simulation", "Idle Clicker"];
-var goalArray = ["Save the Princess", "Gather Resources", "Destroy All Objects on the Screen", "Kill All the Enemies", "Become Rich"];
+var envArray = ["Forest", "Supermarket", "Medieval Castle", "Ocean", "Jungle", "Cave", "City", "Farm", "Beach", "Digital", "Space", "Anime", "Oriental", "SciFi"];
+var genreArray = ["FPS", "Action RPG", "Turn RPG", "Visual Novel", "Top-Down Shooter", "Platformer", "Soccer", "Strategy", "Adventure", "Simulation", "Idle Clicker", "Race", "Metroidvania", "Text-Based", "Multiplayer", "Rythm", "Tower Defense"];
+var goalArray = ["Save the Princess", "Gather Resources", "Destroy All Objects on the Screen", "Kill All the Enemies", "Become Rich", "Punish the Traitors", "Die", "Do Literally Nothing", "Do Literally Everything", "A Bootleg Better than the Original", "Conquest a Nation", "Beat the Record"];
 
 client.on("ready", () => {
     console.log(`Logged as: ${client.user.tag}!`);
